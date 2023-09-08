3d9b2a515d8a5b3f74a0d2224547a29c1c2f8045
karanbro555 <90977560+karanbro555@users.noreply.github.com>
2023-09-08 11:50:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://hinatabot:hinatabot@hinat
 DB_NAME = os.environ.get("DATABASE_NAME", "hinatabot")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001837334577"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001646568491"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
