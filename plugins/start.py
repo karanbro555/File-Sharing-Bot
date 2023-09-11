f0e49f9c4c36bc1681aea05e619802a9e536174e
karanbro555 <90977560+karanbro555@users.noreply.github.com>
2023-09-11 23:58:08 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -85,6 +85,7 @@ async def start_command(client: Client, message: Message):
                 pass
         return
     else:
+        reply_markup = InlineKeyboardMarkup
         await message.reply_text(
             text = START_MSG.format(
                 first = message.from_user.first_name,
